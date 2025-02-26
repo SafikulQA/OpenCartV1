b9934cd95c78ee235a5ce5626a02059035b9549a
Safikul Mondal <safikulmondal@Safikuls-MacBook-Air.local>
2025-02-26 18:09:12 +0530

    26th feb

--- v1/src/test/java/baseClass/BaseTest.java
+++ v1/src/test/java/baseClass/BaseTest.java
@@ -4,8 +4,11 @@ import java.time.Duration;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 
 //Below are the imports for Log4j logging should be added to the BaseTest class file Carefully
 import org.apache.logging.log4j.LogManager;
@@ -14,15 +17,28 @@ import org.apache.logging.log4j.Logger;
 public class BaseTest {
 
      public WebDriver driver;
-     public Logger logger;  //Logger class object for Log4j
+     public Logger logger;  //Logger class object for log4j2
 
     @BeforeMethod
-    //This method will be executed before every test method
-    public void setup() {
-        
-        logger=LogManager.getLogger(this.getClass());
+    @Parameters({"os", "browser"})
+    
+    public void setup(String os, String br)        //This method will be executed before every test method
+     {
+    
+        logger=LogManager.getLogger(this.getClass());       //log4j2
 
-        driver = new ChromeDriver();
+        //driver = new ChromeDriver(); //This is the old way of creating driver object
+        // below code is to create driver object based on the browser parameter passed from testng.xml file
+        switch (br.toLowerCase())
+         {
+            case "chrome":  driver = new ChromeDriver(); break;
+            case "edge":    driver = new EdgeDriver();  break;
+            case "firefox": driver = new FirefoxDriver(); break;
+        
+            default: System.out.println("Please provide correct browser name");
+                return;
+        }
+
         driver.manage().deleteAllCookies();
         driver.get("https://tutorialsninja.com/demo//");
         driver.manage().window().maximize();
